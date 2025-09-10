7f54acdf41cc65fa80a54471feb9d811d857ddb3
Owais-Eltigani <ambidextrous.in@gmail.com>
2025-09-10 21:05:11 +0530

    still configuraing the app

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: './',
   build: {
-    outDir: 'dist-electron',
+    outDir: './dist-electron',
+    // assetsDir: './assets',
+    // rollupOptions: {
+    //   output: {
+    //     entryFileNames: './[name].js',
+    //     chunkFileNames: './[name].js',
+    //     assetFileNames: './[name].[ext]',
+    //   },
+    // },
   },
 });
